fb27b3534ce17c50590f28f6e3f6fe418773c610
jayalakshmiAnoop <44889208+jayalakshmiAnoop@users.noreply.github.com>
2018-11-15 16:40:48 +0530

    3rd commit

--- src/main/java/sample/welcome.java
+++ src/main/java/sample/welcome.java
@@ -4,6 +4,7 @@ public class welcome {
 	public static void main (String args[]){
 		System.out.println("Hi, Welcome");
 		System.out.println("Nice to meet you");
+		System.out.println("Nice to meet you");
 	}
 
 }
